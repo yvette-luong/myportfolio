f06c80fc21bd497bed50cd56a28f3f2cc02d7435
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2021-06-16 01:01:28 -0700

    composer.json update

---
+++ myportfolio/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5c0c1bc7694658de506f687a59b696f1f414ccb8',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5c0c1bc7694658de506f687a59b696f1f414ccb8',
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v5.3.0',
+            'version' => '5.3.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'reference' => '3c2d70f2e64e2922345e89f2ceae47d2463faae1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
